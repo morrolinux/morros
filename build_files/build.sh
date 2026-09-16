f86350220d4382d4e3571ae551f33c5d8236b995
Morrolinux <morrolinux@gmail.com>
2026-09-16 22:48:47 +0200

    fix ffmpeg dev ibs

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,7 +17,9 @@ dnf -y install nautilus kitty mpv gnome-terminal gnome-system-monitor gnome-calc
 
 # OBS and fully-featured ffmpeg with nonfree components from rpm fusion
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 libva-utils --allowerasing
+# dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 libva-utils --allowerasing
+dnf -y swap ffmpeg-free ffmpeg --allowerasing
+dnf -y install ffmpeg-devel x264-libs obs-studio obs-studio-plugin-x264 libva-utils
 
 # Nautilus open any terminal extension
 curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
